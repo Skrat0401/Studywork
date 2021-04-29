f2931c4c8a3e325822f3bf63105f6b904e5f0daf
Skrat0401 <robin.vogelhaus@googlemail.com>
2021-04-29 11:28:29 +0200

    added decoding section

---
+++ Vivado_receiver/Vivado_receiver.srcs/sim_1/new/Testbench_synchronisation.vhd
@@ -0,0 +1,23 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Testbench_synchronisation is
+--  Port ( );
+end Testbench_synchronisation;
+
+architecture Behavioral of Testbench_synchronisation is
+
+begin
+
+
+end Behavioral;
