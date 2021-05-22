b6af3bb46a4e53d20d968cc83dad6603c9bded27
Skrat0401 <robin.vogelhaus@googlemail.com>
2021-05-22 12:42:17 +0200

    upload RanNum testbench

--- FPGA Board emitter/Random_Number_8.vhd
+++ FPGA Board emitter/Random_Number_8.vhd
@@ -14,30 +14,30 @@ entity Random_Number_8 is
 end entity Random_Number_8;
 
 architecture RTL of Random_Number_8 is
-    signal RanNumReg : unsigned(8 downto 1);
-        signal Target_ADDR : unsigned(7 downto 0) := "00000000";
+    signal RanNumReg   : unsigned(8 downto 1);
+    signal Target_ADDR : unsigned(7 downto 0) := "00000000";
 begin
     process(Clk, Rst)
     begin
         if (Rst = '0') then
-            RanNumReg <= KeyIn;
-           Target_ADDR <= "00000000";
+            RanNumReg   <= KeyIn;
+            Target_ADDR <= "00000000";
         elsif (reset_Number = '1') then
-            RanNumReg <= KeyIn;
+            RanNumReg   <= KeyIn;
             Target_ADDR <= "00000000";
         elsif rising_edge(Clk) then
             for ii in 8 downto 2 loop
                 RanNumReg(ii) <= RanNumReg(ii - 1);
             end loop;
             RanNumReg(1) <= RanNumReg(8) XOR RanNumReg(6) XOR RanNumReg(5) XOR RanNumReg(4);
-            if(Target_ADDR < 64)then
+            if (Target_ADDR < 64) then
                 Target_ADDR <= Target_ADDR + 1;
             end if;
         end if;
-    if(Target_ADDR < 64) then
-    RanNum_ADDR_out <= Target_ADDR;
-    RanNum <= RanNumReg;
-    end if;
+        if (Target_ADDR < 64) then
+            RanNum_ADDR_out <= Target_ADDR;
+            RanNum          <= RanNumReg;
+        end if;
     end process;
 
 end architecture RTL;
---
+++ FPGA Board emitter/Testbenches/Testbench_RanNum.vhd
@@ -0,0 +1,63 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Testbench_RanNum is
+end entity Testbench_RanNum;
+
+architecture RTL of Testbench_RanNum is
+
+    component Random_Number_8
+        port(
+            Clk             : in  std_logic;
+            Rst             : in  std_logic;
+            KeyIn           : in  unsigned(7 downto 0);
+            reset_Number    : in  std_logic;
+            RanNum          : out unsigned(8 downto 1);
+            RanNum_ADDR_out : out unsigned(7 downto 0)
+        );
+    end component Random_Number_8;
+
+    signal T_Clk             : std_logic;
+    signal T_Rst             : std_logic;
+    signal T_KeyIn           : unsigned(7 downto 0);
+    signal T_reset_Number    : std_logic;
+    signal T_RanNum          : unsigned(8 downto 1);
+    signal T_RanNum_ADDR_out : unsigned(7 downto 0);
+
+begin
+
+    DUT : component Random_Number_8
+        port map(
+            Clk             => T_Clk,
+            Rst             => T_Rst,
+            KeyIn           => T_KeyIn,
+            reset_Number    => T_reset_Number,
+            RanNum          => T_RanNum,
+            RanNum_ADDR_out => T_RanNum_ADDR_out
+        );
+    
+    T_KeyIn <= "00101101";
+    
+    reset : process
+    begin
+        wait for 5 ns;
+        T_Rst <= '0';
+        wait for 4 ns;
+        T_Rst <= '1';
+        wait for 5 ns;
+        T_Rst <= '0';
+        wait for 4 ns;
+        T_Rst <= '1';
+        wait;
+    end process reset;
+
+    count : process
+    begin
+        T_Clk <= '0';
+        wait for 5 ns;
+        T_Clk <= '1';
+        wait for 5 ns;
+    end process count;
+
+end architecture RTL;
